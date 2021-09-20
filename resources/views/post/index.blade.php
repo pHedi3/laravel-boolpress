e5d129a3290ce41892a4361f1106407d1142fb84
pHedi3 <federico.gualdi3@gmail.com>
2021-09-20 16:35:02 +0200

    modal not work index.blade

--- resources/views/post/index.blade.php
+++ resources/views/post/index.blade.php
@@ -23,12 +23,35 @@
               <td>{{$item->img}}</td>
               <td>
                 <a href="{{route('post.show', $item)}}"><i class="fas fa-search"></i></a>
+                @if (Auth::check())
                 <a href="{{route('post.edit', $item)}}"><i class="fas fa-edit"></i></a>
-                <form action="{{route('post.destroy', $item)}}" method="POST">
+                <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#{{$item->id}}">
+                  <i class="fas fa-trash-alt"></i>
+                </button>
+                <div class="modal fade" id="{{$item->id}}" tabindex="-1" aria-labelledby="{{$item->id}}Label" aria-hidden="true">
+                  <div class="modal-dialog">
+                    <div class="modal-content">
+                      <div class="modal-header">
+                        <h5 class="modal-title" id="{{$item->id}}Label">destroy</h5>
+                        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                      </div>
+                      <div class="modal-body">
+                        are you sure?
+                      </div>
+                      <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" class="btn btn-primary">Destroy</button>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                {{-- <form action="{{route('post.destroy', $item)}}" method="POST">
                   @csrf
                   @method('DELETE')
                   <button type="submit"><i class="fas fa-trash-alt"></i></button>
-                </form>
+                </form> --}}
+                    
+                @endif
               </td>
             </tr>
                 
